b9dae2ddebceddd8a80528c3b3ea7d163d894771
Ilya Subkhankulov <ilya.subkhankulov@gmail.com>
2023-10-19 15:39:11 -0700

    s3 works

--- pipeline/workers/encoding/activities.py
+++ pipeline/workers/encoding/activities.py
@@ -2,7 +2,8 @@ import json
 from typing import Tuple
 
 import boto3
-from common.secret import S3_ACCESS_ID, S3_ACCESS_KEY, S3_BUCKET, S3_REGION
+from botocore.client import ClientError
+from common.secret import S3_ACCESS_ID, S3_ACCESS_KEY, S3_BUCKET, S3_ENDPOINT, S3_REGION
 from temporalio import activity
 from yt_dlp import YoutubeDL
 
@@ -21,6 +22,16 @@ ydl_opts = {
 }
 
 
+def check_bucket_exists(s3_client, bucket_name):
+    bucket_list = s3_client.list_buckets()
+
+    for bucket in bucket_list["Buckets"]:
+        if bucket_name == bucket["Name"]:
+            return True
+
+    return False
+
+
 @activity.defn
 async def download_video(url: str) -> Tuple:
     url = "https://www.youtube.com/watch?v=8ygoE2YiHCs"
@@ -37,10 +48,22 @@ async def download_video(url: str) -> Tuple:
 async def upload_file_to_s3(local_file_path: str) -> str:
     s3_client = boto3.client(
         "s3",
+        endpoint_url=S3_ENDPOINT,
         aws_access_key_id=S3_ACCESS_ID,
         aws_secret_access_key=S3_ACCESS_KEY,
         region_name=S3_REGION,
     )
+    # @todo update client initiation to use dot env
+
+    bucket_exists = check_bucket_exists(s3_client, S3_BUCKET)
+    print(bucket_exists)
+    if not bucket_exists:
+        print("creating bucket")
+        try:
+            s3_client.create_bucket(Bucket=S3_BUCKET)
+        except ClientError as e:
+            print(e)
+            raise e
     with open(local_file_path, "rb") as data:
         s3_client.upload_fileobj(data, S3_BUCKET, local_file_path)
     return "s3://" + S3_BUCKET + "/" + local_file_path
