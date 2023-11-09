96f0943cbd836ccb8a541078aa15a6129aa86b7b
Ilya Subkhankulov <ilyasubkhankulov@users.noreply.github.com>
2023-11-09 12:52:54 -0800

    added min duration check for whisper (#13)

--- pipeline/workers/encoding/transcribe_activity.py
+++ pipeline/workers/encoding/transcribe_activity.py
@@ -3,6 +3,7 @@ import os
 import openai
 from common.params import TranscribeParams
 from dotenv import load_dotenv
+from ffprobe import FFProbe
 from temporalio import activity
 
 load_dotenv()
@@ -17,7 +18,16 @@ async def transcribe(params: TranscribeParams) -> str:
     # By default, the Whisper API only supports files that are less than 25 MB
     # One way to handle this is to use the PyDub
     # open source Python package to split the audio
-    audio_file = open(params.audio_file_url, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print('transcript', transcript)
-    return transcript['text']
+
+    audio_file = params.audio_file_url
+    # Use ffprobe to get the duration of the audio file
+    metadata = FFProbe(audio_file)
+    duration = float(metadata.streams[0].duration)
+    # If the audio file is less than 0.1 seconds, return an empty string
+    if duration < 0.1:
+        return ""
+    else:
+        audio_file = open(audio_file, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        print('transcript', transcript)
+        return transcript['text']
